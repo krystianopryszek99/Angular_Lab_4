81d3da5e415ac3b0be3e110eabdaaea04a07679f
KRYSTIAN OPRYSZEK - STUDENT <G00366895@gmit.ie>
2020-02-18 17:42:31 +0000

    Lab4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding-app';
+  count: number = 0;
+  hideLabel:boolean = true;
+  
+  buttonPressed(){
+    console.log("Button Pressed!");
+    this.count++;
+  }
+
+  imageClicked(){
+    console.log("Double click!");
+    if(this.hideLabel ==true){
+      this.hideLabel =false;
+    }
+    else 
+    {
+      this.hideLabel = true;
+    }
+  }
 }
